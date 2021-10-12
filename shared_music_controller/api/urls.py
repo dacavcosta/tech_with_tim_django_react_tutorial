e6bfcfe224f80c07574fee5b10b6e09494c8ddca
dacavcosta <dcavalcanticosta@gmail.com>
2021-10-12 18:52:52 -0300

    Created the GetRoom view and defined a Get Method to return the room data to the frontend Added get-room path to urls.py

--- shared_music_controller/api/urls.py
+++ shared_music_controller/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateRoomView, RoomView
+from .views import CreateRoomView, GetRoom, RoomView
 
 urlpatterns = [
     path('room', RoomView.as_view()),
-    path('create-room', CreateRoomView.as_view())
+    path('create-room', CreateRoomView.as_view()),
+    path('get-room', GetRoom.as_view())
 ]
