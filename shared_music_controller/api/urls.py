1b4137c25c2b3b76b678984791ca727985b00979
dacavcosta <dcavalcanticosta@gmail.com>
2021-10-14 17:40:49 -0300

    Created the UpdateRoomSerializer in Serializers.py Created the View UpdateRoom in Views.py Added the update-room path to urls.py

--- shared_music_controller/api/urls.py
+++ shared_music_controller/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateRoomView, GetRoomView, JoinRoomView, RoomView, UserInRoom, LeaveRoom
+from .views import CreateRoomView, GetRoomView, JoinRoomView, RoomView, UserInRoom, LeaveRoom, UpdateRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('join-room', JoinRoomView.as_view()),
     path('user-in-room', UserInRoom.as_view()),
     path('leave-room', LeaveRoom.as_view()),
+    path('update-room', UpdateRoom.as_view()),
 ]
