e1630e34d18ce9ba1d64a9643997dc2f676bd943
dacavcosta <dcavalcanticosta@gmail.com>
2021-10-21 16:26:52 -0300

    Implemented the call to the spotfy route in room.js fixed GET issue on views.py added the redirect_uri in creadentials.py

--- shared_music_controller/spotfy/views.py
+++ shared_music_controller/spotfy/views.py
@@ -19,31 +19,31 @@ class AuthUrl(APIView):
 
         return Response({'url': url}, status=status.HTTP_200_OK)
 
-    def spotfy_callback(request, format=None):
-        code = request.Get.get('code')
-        error = request.Get.get('error')
+def spotfy_callback(request, format=None):
+    code = request.GET.get('code')
+    error = request.GET.get('error')
 
-        response = post(spotify_api, data={
-            'grant_type': 'authorization_code',
-            'code': code,
-            'redirect_uri': REDIRECT_URI,
-            'client_id': CLIENT_ID,
-            'client_secret': CLIENT_SECRET
-        }).json()
+    response = post(spotify_api, data={
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': REDIRECT_URI,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
+    }).json()
 
-        access_token = response.get('access_token')
-        token_type = response.get('token_type')
-        refresh_token = response.get('refresh_token')
-        expires_in = response.get('expires_in')
-        error = response.get('error')
+    access_token = response.get('access_token')
+    token_type = response.get('token_type')
+    refresh_token = response.get('refresh_token')
+    expires_in = response.get('expires_in')
+    error = response.get('error')
 
-        if not request.session.exists(request.session.session_key):
-            request.session.create()
+    if not request.session.exists(request.session.session_key):
+        request.session.create()
 
-        update_or_create_user_tokens(
-            request.session.session_key, access_token, refresh_token, token_type, expires_in)
-        
-        return redirect('frontend:')
+    update_or_create_user_tokens(
+        request.session.session_key, access_token, refresh_token, token_type, expires_in)
+    
+    return redirect('frontend:')
     
 class IsAuthenticated(APIView):
     def get(self, request, format=None):
